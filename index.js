abd7eef4b186edc4ce377bf1ad618fe18b7c5734
Ivan Balashov <16803852+IvanParadox@users.noreply.github.com>
2019-11-03 20:54:46 +0400

    Added data

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const MAX_STOCK_PRICE = 0;
 const MAX_LOSE_YEAR = 0;
 const MAX_LOSE_MONTH = 0;
 
-let data = require ("./data/index.json");
+let data = require ('./data/index.json');
 
-console.log(data["amzn"][country]);
+console.log(data['amzn']['country']);

4d9fe38457ab8d50d6027827b6823d66cd5de9e3
Ivan Balashov <16803852+IvanParadox@users.noreply.github.com>
2019-11-06 14:36:40 +0300

    update

--- music/search.js
+++ music/search.js
@@ -1,4 +1,12 @@
-let artists = [require('./artists/1.json'), require('./artists/2.json'), require('./artists/3.json')];
-let songs = [require('./songs/1.json'), require('./songs/2.json'), require('./songs/3.json')];
+let data_artists=[];
+let data_songs=[];
 
-console.log(artists[0], songs[0]);
+data_artists[0]=require('./artists/1.json');
+data_artists[1]=require('./artists/2.json');
+data_artists[2]=require('./artists/3.json');
+
+data_songs[0]=require('./songs/1.json');
+data_songs[1]=require('./songs/2.json');
+data_songs[2]=require('./songs/3.json');
+
+console.log(data_artists[1], data_songs[1]);

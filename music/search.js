917958b1b2a91eb85b52a8b26862718eb78e736f
Ivan Balashov <16803852+IvanParadox@users.noreply.github.com>
2019-11-12 22:53:22 +0300

    big update

--- music/search.js
+++ music/search.js
@@ -1,12 +1,37 @@
+fs=require("fs");
 let data_artists=[];
 let data_songs=[];
 
-data_artists[0]=require('./artists/1.json');
-data_artists[1]=require('./artists/2.json');
-data_artists[2]=require('./artists/3.json');
+function loadData() {
+  try {
+    artists_read = fs.readdirSync("./music/artists");
+    songs_read = fs.readdirSync("./music/songs");
+    console.log(artists_read, songs_read);
+  } catch(err) {
+    console.log('Вынь голову из жопы, у тебя ошибка!');
+  }
+  return
 
-data_songs[0]=require('./songs/1.json');
-data_songs[1]=require('./songs/2.json');
-data_songs[2]=require('./songs/3.json');
+  for (let value of artists_read) {
+    let artists_values = JSON.parse(fs.readFileSync(`./music/artists/${value}`));
+    data_artists.push(artists_values);
+  }
 
-console.log(data_songs);
+  for (let value of songs_read) {
+    let songs_values = JSON.parse(fs.readFileSync(`./music/songs/${value}`));
+    data_songs.push(songs_values);
+  }
+
+  let data=[data_artists, data_songs];
+  return data;
+}
+
+function saveData (data) {
+  fs.writeFile('music.txt', JSON.stringify(data), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+}
+
+let loadedData = loadData();
+saveData(loadedData);

b7c19d33003d9e1b2ec29592c6f0b8870c110999
Ivan Balashov <16803852+IvanParadox@users.noreply.github.com>
2019-11-22 18:01:59 +0300

    Update

--- music/search.js
+++ music/search.js
@@ -1,8 +1,10 @@
 fs=require("fs");
-let data_artists=[];
-let data_songs=[];
 
 function loadData() {
+
+  let data_artists=[];
+  let data_songs=[];
+
   try {
     artists_read = fs.readdirSync("./music/artists");
     songs_read = fs.readdirSync("./music/songs");
@@ -10,7 +12,6 @@ function loadData() {
   } catch(err) {
     console.log('Вынь голову из жопы, у тебя ошибка!');
   }
-  return
 
   for (let value of artists_read) {
     let artists_values = JSON.parse(fs.readFileSync(`./music/artists/${value}`));
@@ -22,16 +23,40 @@ function loadData() {
     data_songs.push(songs_values);
   }
 
-  let data=[data_artists, data_songs];
-  return data;
+  return {data_artists, data_songs};
 }
 
-function saveData (data) {
-  fs.writeFile('music.txt', JSON.stringify(data), (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+    let requestedFile = decodeURI(request.url);
+    let mypage = fs.readFileSync(`./music/web/index.html`);
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    console.log(requestedFile);
+    try {
+      response.statuscode = 200;
+      response.end(mypage);
+    } catch (err) {
+      response.statuscode = 404;
+      response.end(`Запрашиваемого файла не существует`)
+    }
 }
 
-let loadedData = loadData();
-saveData(loadedData);
+//function saveData (artists, songs) {
+  //fs.writeFile('music.txt', JSON.stringify({artists, songs}), (err) => {
+    //if (err) throw err;
+    //console.log('The file has been saved!');
+  //});
+//}
+
+//let loadedData = loadData();
+//saveData(loadedData.data_artists, loadedData.data_songs);
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }    console.log(`server is listening on ${port}`);
+})
